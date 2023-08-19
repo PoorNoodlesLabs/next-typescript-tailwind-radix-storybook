ee2810c0e80f8ac26a7fec078473ef6efe8163ea
Travis Ueki <travueki@gmail.com>
2023-08-19 16:52:35 -0500

    Add dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@radix-ui/themes";
+import {
+  Button,
+  DialogRoot,
+  DialogTrigger,
+  DialogTitle,
+  DialogContent,
+} from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
+      <DialogRoot>
+        <DialogTrigger>
+          <Button>Hello World</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Hello World</DialogTitle>
+        </DialogContent>
+      </DialogRoot>
     </main>
   );
 }
